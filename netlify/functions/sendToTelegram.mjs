2a943876ae34faa8f5d56bf85ea4278b3c1ea48b
Foxxich <64288356+Foxxich@users.noreply.github.com>
2024-08-07 12:47:29 +0200

    Updated text

--- netlify/functions/sendToTelegram.mjs
+++ netlify/functions/sendToTelegram.mjs
@@ -1,30 +1,34 @@
 // netlify/functions/sendToTelegram.mjs
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export async function handler(event, context) {
-  console.log('Function is invoked');
+  console.log("Function is invoked");
   const { name, email, phone, message } = JSON.parse(event.body);
 
-  console.log('Received data:', { name, email, phone, message });
+  console.log("Received data:", { name, email, phone, message });
 
   const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
   const telegramChatId = process.env.TELEGRAM_CHAT_ID;
 
-  console.log('Using Telegram Token and Chat ID:', telegramToken, telegramChatId);
+  console.log(
+    "Using Telegram Token and Chat ID:",
+    telegramToken,
+    telegramChatId
+  );
 
-  const text = `New contact form submission:
-    Name: ${name}
-    Email: ${email}
-    Phone: ${phone}
-    Message: ${message}`;
+  const text = `Новий користувач:\n
+Ім'я: ${name}\n
+Email: ${email}\n
+Телефон: ${phone}\n
+Повідомлення: ${message}\n`;
 
   const url = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
 
   try {
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         chat_id: telegramChatId,
@@ -34,7 +38,7 @@ export async function handler(event, context) {
 
     const data = await response.json();
 
-    console.log('Response from Telegram:', data);
+    console.log("Response from Telegram:", data);
 
     if (!data.ok) {
       throw new Error(data.description);
@@ -42,10 +46,10 @@ export async function handler(event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Message sent to Telegram' }),
+      body: JSON.stringify({ message: "Message sent to Telegram" }),
     };
   } catch (error) {
-    console.error('Error sending message to Telegram:', error);
+    console.error("Error sending message to Telegram:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
