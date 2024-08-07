e54dfeba3096e60a3c28d03c3fd5df4046be581f
Foxxich <64288356+Foxxich@users.noreply.github.com>
2024-08-07 12:37:57 +0200

    updated front

--- src/App.js
+++ src/App.js
@@ -96,19 +96,24 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('/.netlify/functions/sendToTelegram', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, phone, message }),
-    });
-    const data = await response.json();
-    console.log(data.message);
-    setFormSubmitted(true);
-    setShowSuccessMessage(true);
-    setTimeout(() => setShowSuccessMessage(false), 5000);
-  };  
+    try {
+      const response = await fetch('/.netlify/functions/sendToTelegram', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, phone, message }),
+      });
+      const data = await response.json();
+      console.log(data.message);
+      setFormSubmitted(true);
+      setShowSuccessMessage(true);
+      setTimeout(() => setShowSuccessMessage(false), 5000);
+    } catch (error) {
+      console.error('Error sending form data:', error);
+    }
+  };
+  
 
   return (
     <div className="App">
